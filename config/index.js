191976684f169ca0930ea5611348fe3af3d60530
wangzhizhen1998 <379279146@qq.com>
2022-09-21 06:56:58 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "",
+  APP_ID: "wx0b0521a2aa416ee9",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "",
+  APP_SECRET: "0c11e0e0ebcdd367320d996790cdb2d5",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -95,16 +95,16 @@ export const config = {
       ]
     },
     {
-      name: "老婆1",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      horoscopeDate: '',
-      horoscopeDateType: '',
+      name: "宝贝小张",
+      id: "o3hT55-lMTXYJWoyeemjbL98dWeg",
+      useTemplateId: "JuKzZYULZ3dEE8PwcI0ojLQEAN7Cv2Twp16dxnEcIdg",
+      province: "贵州",
+      city: "贵阳",
+      horoscopeDate: '09-24',
+      horoscopeDateType: '今日',
       openUrl: "https://wangxinleo.cn",
-      festivals: [],
-      customizedDateList: []
+      festivals: [{"type": "生日", "name": "小张", "year": "2000", "date": "09-24"}],
+      customizedDateList: [{"keyword": "love_day", date: "2022-06-05"}]
     },
     {
       name: "老婆2",
@@ -139,7 +139,7 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "",
+  CALLBACK_TEMPLATE_ID: "JuKzZYULZ3dEE8PwcI0ojLQEAN7Cv2Twp16dxnEcIdg",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -147,7 +147,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "o3hT55-lMTXYJWoyeemjbL98dWeg	",
     },
     // 你可以不断按格式往下增加
     // ...
